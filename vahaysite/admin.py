929258e84eb617d2d0f52843b1fa0a8dad56efa5
riidatibula <riidatibula@gmail.com>
2017-05-09 16:46:46 +0800

    added review model

--- vahaysite/admin.py
+++ vahaysite/admin.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Vahay
+from .models import Vahay, Review
 
 admin.site.register(Vahay)
+admin.site.register(Review)
--- vahaysite/models.py
+++ vahaysite/models.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 class Vahay(models.Model):
-	owner = models.ForeignKey(get_user_model())
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=255)
 	rent_range = models.CharField(max_length=100)
 	category = models.CharField(max_length=100)
@@ -19,3 +19,12 @@ class Vahay(models.Model):
 
 	def __str__(self):
 		return self.name + " - " + self.location
+
+
+class Review(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	comment = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.user.username + " - " + self.comment
+
